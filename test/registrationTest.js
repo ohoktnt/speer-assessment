a90e4e409f1bebb072710ad829e6a77922980cad
Tammy <tammy.n.tran@gmail.com>
2021-03-06 10:13:23 -0500

    refractoring

--- test/registrationTest.js
+++ test/registrationTest.js
@@ -7,7 +7,7 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 describe('Testing Registration', () => {
-  xit('should return all users on /users GET', function(done) {
+  it('should return all users on /users GET', function(done) {
     chai.request('http://localhost:8002')
         .get('/users')
         .end(function(err, res) {
@@ -18,7 +18,7 @@ describe('Testing Registration', () => {
         })
   })
 
-  xit('should successfully register with unique username on /users POST', function(done) {
+  it('should successfully register with unique username on /users POST', function(done) {
     chai.request('http://localhost:8002')
         .post('/users')
         .type('form')
@@ -36,7 +36,7 @@ describe('Testing Registration', () => {
         })
   })
 
-  xit('should not register user with existing username on /users POST', function(done) {
+  it('should not register user with existing username on /users POST', function(done) {
     chai.request('http://localhost:8002')
       .post('/users')
       .type('form')
